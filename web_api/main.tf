650e62e24759ccb011f51ee29bf5e676419c65c7
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2022-06-09 00:28:19 +0900

    iamのコードを追加

---
+++ web_api/main.tf
@@ -0,0 +1,30 @@
+# module "check_function" {
+#   source = "./lambda_function"
+#   function_name = "check-function"
+
+# }
+
+
+# variable "role" {
+#   type = string
+# }
+
+# variable "filename" {
+#   type = string
+# }
+
+# variable "archive_filename" {
+#   type = string
+# }
+
+# variable "execution_arn" {
+#   type = string
+# }
+
+# variable "method" {
+#   type = string
+# }
+
+# variable "path" {
+#   type = string
+# }
